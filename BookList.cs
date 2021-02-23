bf07b7a13b12cff63bd4befae3008ec62a080e43
Gabriel Vega <gvega001@gmail.com>
2021-02-22 18:03:47 -0800

    Class for book list created.

---
+++ BookList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Generics
+{
+    public class BookList
+    {
+        public void Add(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+        
+    }
+
+    public class ObjectList
+    {
+        public void Add(object value)
+        {
+
+        }
+
+        public object this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
--- List.cs
+++ List.cs
@@ -1,7 +1,17 @@
-﻿namespace Generics
+﻿using System;
+
+namespace Generics
 {
     public class List
     {
-        
+        public void Add(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
     }
 }
