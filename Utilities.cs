807799e6c7b175d965effb959363f56c9df5434a
Gabriel Vega <gvega001@gmail.com>
2021-02-22 19:44:35 -0800

    Compared a utility class to find max int. Generic Class and generic method.

--- Utilities.cs
+++ Utilities.cs
@@ -1,7 +1,18 @@
-﻿namespace Generics
-{
-    public class Utilities
-    {
+﻿using System;
+using System.Security.AccessControl;
 
+namespace Generics
+{
+    public class Utilities<T> where T: IComparable
+    {
+        public int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        public T Max(T a, T b) 
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
     }
 }
